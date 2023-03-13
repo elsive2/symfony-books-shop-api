8abf108c585bd48d55ba0aba515272bfec90eb16
elsive2 <maximyakovlev228@gmail.com>
2023-03-14 00:22:31 +0600

    feat: Swagger set up for category controller

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Model\CategoryListResponse;
 use App\Service\CategoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use OpenApi\Annotations\Response;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 #[Route('/api/v1')]
 class CategoryController extends AbstractController
@@ -14,7 +17,14 @@ class CategoryController extends AbstractController
         private CategoryService $categoryService 
     ) { }
 
-    #[Route('/categories', name: 'category_index')]
+    /**
+     * @Response(
+     *  response=200,
+     *  description="Return book categories",
+     *  @Model(type=CategoryListResponse::class)
+     * )
+     */
+    #[Route('/categories', name: 'category_index', methods: ['GET'])]
     public function index(): JsonResponse
     {
         $categories = $this->categoryService->getCategories();
