0a8f985d4279103e639097444d5a11cd48420084
elsive2 <maximyakovlev228@gmail.com>
2023-03-14 20:34:33 +0600

    feat: Make fixtures for books and categories

---
+++ src/DataFixtures/BookFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Book;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class BookFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $androidCategory = $this->getReference(CategoryFixture::ANDROID_CATEGORY);
+        $dataCategory = $this->getReference(CategoryFixture::DATA_CATEGORY);
+        $networkCategory = $this->getReference(CategoryFixture::NETWORK_CATEGORY);
+        
+        $books = [
+            (new Book)
+                ->setTitle('Rxjava for Android Developers')
+                ->setPublicationDate(new \DateTime('2019-04-19'))
+                ->setMeap(false)
+                ->setAuthors(['Timo Tuominen'])
+                ->setSlug('rxjava-for-android-developers')
+                ->addCategory($androidCategory)
+                ->addCategory($dataCategory)
+                ->setImage('https://books.google.kz/books/publisher/content?id=ajszEAAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&imgtk=AFLRE738uMOnevzEu8ZAN2Lrxzn__9nXAlhNqgGI5vBOUmBtl41J1Zm8khFCJXI06VNN7I3Gfsm-he9kjZ9SyDZZBLNJ3gHj0T79v23wTkdAj_7-CYnXbhqNWb3voTE8L6eFz9f3qIn8')
+        ];
+
+        foreach ($books as $book) {
+            $manager->persist($book);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixture::class
+        ];
+    }
+}
--- src/DataFixtures/CategoryFixture.php
+++ src/DataFixtures/CategoryFixture.php
@@ -8,12 +8,22 @@ use Doctrine\Persistence\ObjectManager;
 
 class CategoryFixture extends Fixture
 {
+    public const ANDROID_CATEGORY = 'android';
+    public const DATA_CATEGORY = 'data';
+    public const NETWORK_CATEGORY = 'network';
+
     public function load(ObjectManager $manager): void
     {
-        $manager->persist((new Category)->setTitle('Android')->setSlug('android'));
-        $manager->persist((new Category)->setTitle('Data')->setSlug('data'));
-        $manager->persist((new Category)->setTitle('Network')->setSlug('network'));
-
+        $categories = [
+            self::ANDROID_CATEGORY => (new Category)->setTitle('Android')->setSlug('android'),
+            self::DATA_CATEGORY => (new Category)->setTitle('Data')->setSlug('data'),
+            self::NETWORK_CATEGORY => (new Category)->setTitle('Network')->setSlug('network')
+        ];
+        
+        foreach ($categories as $key => $category) {
+            $manager->persist($category);
+            $this->addReference($key, $category);
+        }
         $manager->flush();
     }
 }
