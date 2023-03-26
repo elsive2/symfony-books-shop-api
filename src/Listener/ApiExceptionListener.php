51b7165be983296d92d65acd5bdf13778921436b
elsive2 <maximyakovlev228@gmail.com>
2023-03-26 23:12:39 +0600

    feat: Base set up for refresh tokens

--- src/Listener/ApiExceptionListener.php
+++ src/Listener/ApiExceptionListener.php
@@ -7,11 +7,14 @@ use App\Model\ErrorResponse;
 use App\Service\ExceptionHandler\ExceptionMapping;
 use App\Service\ExceptionHandler\ExceptionMappingResolver;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
+use Throwable;
 
 class ApiExceptionListener
 {
@@ -26,6 +29,10 @@ class ApiExceptionListener
     {
         $throwable = $event->getThrowable();
         $mapping = $this->resolver->resolve(get_class($throwable));
+        
+        if ($this->isSecurityException($throwable)) {
+            return;
+        }
 
         if (null === $mapping) {
             $mapping = ExceptionMapping::fromCode(Response::HTTP_INTERNAL_SERVER_ERROR);
@@ -44,5 +51,11 @@ class ApiExceptionListener
         $response = new JsonResponse($data, $mapping->getCode(), [], true);
 
         $event->setResponse($response);
+    }
+
+    private function isSecurityException(Throwable $throwable)
+    {
+        return $throwable instanceof AuthenticationException
+            || $throwable instanceof AccessDeniedException;
     }
 }
